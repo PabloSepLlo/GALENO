c13a36aad723fd6a683de59515d12a5e14fc4c0a
Pablo <pablosepulveda31@gmail.com>
2025-05-03 20:38:53 +0200

    wip

--- servicios/consultas_ingreso.php
+++ servicios/consultas_ingreso.php
@@ -11,9 +11,169 @@
                 SELECT 
                     COUNT(*) AS n_ingreso 
                 FROM ingreso
-                WHERE fecha_ingreso BETWEEN :inicio AND :fin
+                WHERE i.fecha_ingreso <= :fin
+                AND (i.fecha_alta IS NULL OR i.fecha_alta >= :inicio)
+            ");
+        }
+        public function get_datos_ingresos() {
+            $stmt = $this->pdo->prepare("
+                SELECT 
+                    ROUND(SUM(CASE WHEN i.reingreso = 'SÍ' THEN 1 ELSE 0 END) * 100 / NULLIF(COUNT(i.reingreso), 0), 2) AS reingreso,
+                    SUM(i.analitica) AS analiticas,
+                    SUM(i.eco) AS eco,
+                    SUM(i.minimental) AS minimental,
+                    SUM(i.cultivo) AS cultivo,
+                    SUM(i.num_visit) AS num_visit,
+                    ROUND(SUM(CASE WHEN i.crm = 0 OR i.crm = 1 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.crm IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS crm_0_1,
+                    ROUND(SUM(CASE WHEN i.crm = 2 OR i.crm = 3 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.crm IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS crm_2_3,
+                    ROUND(SUM(CASE WHEN i.crm = 4 OR i.crm = 5 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.crm IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS crm_4_5,
+                    ROUND(SUM(CASE WHEN i.barthel < 45 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.barthel IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS barthel_menos_45,
+                    ROUND(SUM(CASE WHEN i.barthel >= 1 AND i.barthel <= 59 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.barthel IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS barthel_45_59,
+                    ROUND(SUM(CASE WHEN i.barthel >= 60 AND i.barthel <= 80 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.barthel IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS barthel_60_80,
+                    ROUND(SUM(CASE WHEN i.barthel > 80 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.barthel IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS barthel_mas_80,
+                    ROUND(SUM(CASE WHEN i.crf = 0 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.crf IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS crf_0,
+                    ROUND(SUM(CASE WHEN i.crf = 1 OR i.crf = 2 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.crf IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS crf_1_2,
+                    ROUND(SUM(CASE WHEN i.crf = 3 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.crf IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS crf_3,
+                    ROUND(SUM(CASE WHEN i.crf = 4 OR i.crf = 5 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.crf IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS crf_4_5,
+                    ROUND(SUM(CASE WHEN i.pfeiffer <= 4 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.pfeiffer IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS pfeiffer_menor_4,
+                    ROUND(SUM(CASE WHEN i.pfeiffer > 4 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN i.pfeiffer IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS pfeiffer_mas_4
+                FROM ingreso AS i
+                JOIN (
+                    SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                    FROM ingreso
+                    GROUP BY nhc
+                ) AS ultimos 
+                ON i.nhc = ultimos.nhc AND i.fecha_ingreso = ultimos.fecha_max
+                WHERE i.fecha_ingreso <= :fin
+                AND (i.fecha_alta IS NULL OR i.fecha_alta >= :inicio);
+            ");
+        }
+        
+        public function get_ingreso_pr() {
+            $stmt = $this->pdo->prepare("
+                SELECT 
+                    pr.descripcion AS procedencia,
+                    ROUND(COUNT(*) * 100.0 / (
+                        SELECT COUNT(*)
+                        FROM ingreso AS i2
+                        JOIN (
+                            SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                            FROM ingreso
+                            GROUP BY nhc
+                        ) AS ultimos2 
+                        ON i2.nhc = ultimos2.nhc AND i2.fecha_ingreso = ultimos2.fecha_max
+                        WHERE i2.fecha_ingreso <= :fin
+                        AND (i2.fecha_alta IS NULL OR i2.fecha_alta >= :inicio)
+                    ), 2) AS porcentaje
+                FROM ingreso AS i
+                JOIN (
+                    SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                    FROM ingreso
+                    GROUP BY nhc
+                ) AS ultimos 
+                    ON i.nhc = ultimos.nhc AND i.fecha_ingreso = ultimos.fecha_max
+                JOIN procedencia AS pr ON i.id_procedencia = pr.id_procedencia
+                WHERE i.fecha_ingreso <= :fin
+                AND (i.fecha_alta IS NULL OR i.fecha_alta >= :inicio)
+                GROUP BY pr.descripcion;
+            ");
+        }
+        public function get_ingreso_mi() {
+            $stmt = $this->pdo->prepare("
+                SELECT 
+                    mi.descripcion AS motivo_ingreso,
+                    ROUND(COUNT(*) * 100.0 / (
+                        SELECT COUNT(*)
+                        FROM ingreso AS i2
+                        JOIN (
+                            SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                            FROM ingreso
+                            GROUP BY nhc
+                        ) AS ultimos2 
+                        ON i2.nhc = ultimos2.nhc AND i2.fecha_ingreso = ultimos2.fecha_max
+                        WHERE i2.fecha_ingreso <= :fin
+                        AND (i2.fecha_alta IS NULL OR i2.fecha_alta >= :inicio)
+                    ), 2) AS porcentaje
+                FROM ingreso AS i
+                JOIN (
+                    SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                    FROM ingreso
+                    GROUP BY nhc
+                ) AS ultimos 
+                    ON i.nhc = ultimos.nhc AND i.fecha_ingreso = ultimos.fecha_max
+                JOIN motivo_ingreso AS mi ON i.id_motivo_ingreso = mi.id_motivo_ingreso
+                WHERE i.fecha_ingreso <= :fin
+                AND (i.fecha_alta IS NULL OR i.fecha_alta >= :inicio)
+                GROUP BY mi.motivo_ingreso;
             ");
         }
 
+        public function get_ingreso_destino() {
+            $stmt = $this->pdo->prepare("
+                SELECT 
+                    de.descripcion AS destino,
+                     ROUND(COUNT(*) * 100.0 / (
+                        SELECT COUNT(*)
+                        FROM ingreso AS i2
+                        JOIN (
+                            SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                            FROM ingreso
+                            GROUP BY nhc
+                        ) AS ultimos2 
+                        ON i2.nhc = ultimos2.nhc AND i2.fecha_ingreso = ultimos2.fecha_max
+                        WHERE i2.fecha_ingreso <= :fin
+                        AND (i2.fecha_alta IS NULL OR i2.fecha_alta >= :inicio)
+                    ), 2) AS porcentaje
+                FROM ingreso AS i
+                JOIN (
+                    SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                    FROM ingreso
+                    GROUP BY nhc
+                ) AS ultimos 
+                    ON i.nhc = ultimos.nhc AND i.fecha_ingreso = ultimos.fecha_max
+                JOIN destino AS de ON i.id_destino = de.id_destino
+                WHERE i.fecha_ingreso <= :fin
+                AND (i.fecha_alta IS NULL OR i.fecha_alta >= :inicio)
+                GROUP BY de.motivo_ingreso;
+            ");
+        }
+        public function get_porcentaje_muerte_domicilio() {
+            $stmt = $this->pdo->prepare("
+                SELECT 
+                    ROUND(COUNT(CASE WHEN i.rip_domi = 'SÍ' THEN 1 ELSE NULL END) * 100.0 / (
+                        SELECT COUNT(*)
+                        FROM ingreso AS i2
+                        JOIN (
+                            SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                            FROM ingreso
+                            GROUP BY nhc
+                        ) AS ultimos2 
+                        ON i2.nhc = ultimos2.nhc AND i2.fecha_ingreso = ultimos2.fecha_max
+                        WHERE i2.fecha_ingreso <= :fin
+                        AND (i2.fecha_alta IS NULL OR i2.fecha_alta >= :inicio)
+                    ), 2) AS porcentaje
+                FROM ingreso AS i
+                JOIN (
+                    SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                    FROM ingreso
+                    GROUP BY nhc
+                ) AS ultimos 
+                    ON i.nhc = ultimos.nhc AND i.fecha_ingreso = ultimos.fecha_max
+                JOIN destino AS de ON i.id_destino = de.id_destino
+                WHERE i.fecha_ingreso <= :fin
+                AND (i.fecha_alta IS NULL OR i.fecha_alta >= :inicio)
+                AND i.id_destino = 8;
+            ");
+        }
+        public function get_media_dias_ingreso() {
+            $stmt = $this->pdo->prepare("
+                SELECT 
+                    AVG(DATEDIFF(fecha_alta, fecha_ingreso)) as get_media_dias_ingreso
+                FROM ingreso
+                WHERE fecha_ingreso <= :fin
+                AND (fecha_alta >= :inicio)
+                AND fecha_alta IS NOT NULL;
+            ");
+        }
     }
 ?>
--- servicios/consultas_paciente.php
+++ servicios/consultas_paciente.php
@@ -10,41 +10,65 @@
             $stmt = $this->pdo->prepare("
                 SELECT 
                     COUNT(*) AS n_pacientes, 
-                    AVG(edad) AS edad_media,
-                    ROUND(SUM(CASE WHEN sexo = 'F' THEN 1 ELSE 0 END) * 100 / n_pacientes, 2) AS mujeres,
-                    ROUND(SUM(CASE WHEN sexo = 'M' THEN 1 ELSE 0 END) * 100 / n_pacientes, 2) AS hombres
-                    ROUND(SUM(CASE WHEN incontinencia_urinaria = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(incontinencia_urinaria), 0), 2) AS iu,
-                    ROUND(SUM(CASE WHEN incontinencia_fecal = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(incontinencia_fecal), 0), 2) AS if,
-                    ROUND(SUM(CASE WHEN insomnio = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(insomnio), 0), 2) AS insomnio,
-                    ROUND(SUM(CASE WHEN dolor = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(dolor), 0), 2) AS dolor,
-                    ROUND(SUM(CASE WHEN disfagia = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(disfagia), 0), 2) AS disfagia,
-                    ROUND(SUM(CASE WHEN grado_ulcera IS NOT NULL THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(*), 0), 2) AS ulcera_total,
-                    ROUND(SUM(CASE WHEN grado_ulcera = 1 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN grado_ulcera IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS ulcera_1,
-                    ROUND(SUM(CASE WHEN grado_ulcera = 2 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN grado_ulcera IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS ulcera_2,
-                    ROUND(SUM(CASE WHEN grado_ulcera = 3 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN grado_ulcera IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS ulcera_3,
-                    ROUND(SUM(CASE WHEN grado_ulcera = 4 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN grado_ulcera IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS ulcera_4
-                FROM datos_paciente
-                WHERE fecha_ingreso BETWEEN :inicio AND :fin
+                    AVG(dp.edad) AS edad_media,
+                    ROUND(SUM(CASE WHEN dp.sexo = 'F' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS mujeres,
+                    ROUND(SUM(CASE WHEN dp.sexo = 'M' THEN 1 ELSE 0 END) * 100.0 / COUNT(*), 2) AS hombres,
+                    ROUND(SUM(CASE WHEN dp.in_ur = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(dp.in_ur), 0), 2) AS iu,
+                    ROUND(SUM(CASE WHEN dp.in_fec = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(dp.in_fec), 0), 2) AS ifec,
+                    ROUND(SUM(CASE WHEN dp.insom = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(dp.insom), 0), 2) AS insomnio,
+                    ROUND(SUM(CASE WHEN dp.dolor = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(dp.dolor), 0), 2) AS dolor,
+                    ROUND(SUM(CASE WHEN dp.disf = 'SÍ' THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(dp.disf), 0), 2) AS disfagia,
+                    ROUND(SUM(CASE WHEN dp.grado_ulcera IS NOT NULL THEN 1 ELSE 0 END) * 100.0 / NULLIF(COUNT(*), 0), 2) AS ulcera_total,
+                    ROUND(SUM(CASE WHEN dp.grado_ulcera = 1 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN dp.grado_ulcera IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS ulcera_1,
+                    ROUND(SUM(CASE WHEN dp.grado_ulcera = 2 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN dp.grado_ulcera IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS ulcera_2,
+                    ROUND(SUM(CASE WHEN dp.grado_ulcera = 3 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN dp.grado_ulcera IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS ulcera_3,
+                    ROUND(SUM(CASE WHEN dp.grado_ulcera = 4 THEN 1 ELSE 0 END) * 100.0 / NULLIF(SUM(CASE WHEN dp.grado_ulcera IS NOT NULL THEN 1 ELSE 0 END), 0), 2) AS ulcera_4
+                FROM datos_paciente dp
+                JOIN ingreso i ON dp.nhc = i.nhc
+                WHERE i.fecha_ingreso <= '2025-12-31'
+                AND (i.fecha_alta IS NULL OR i.fecha_alta >= '2024-01-01');
                 ");
         }
         public function get_paciente_cs() {
             $stmt = $this->pdo->prepare("
                 SELECT 
-                    cs.descripcion AS centro_salud,
-                    COUNT(*) AS num_pacientes
+                    cs.codigo_centro AS centro_salud,
+                    ROUND(COUNT(*) * 100.0 / (
+                        SELECT COUNT(*)
+                        FROM ingreso AS i2
+                        JOIN (
+                            SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                            FROM ingreso
+                            GROUP BY nhc
+                        ) AS ultimos2 
+                        ON i2.nhc = ultimos2.nhc AND i2.fecha_ingreso = ultimos2.fecha_max
+                        WHERE i2.fecha_ingreso <= '2025-12-31'
+                        AND (i2.fecha_alta IS NULL OR i2.fecha_alta >= '2024-12-31')
+                    ), 2) AS porcentaje
                 FROM datos_paciente AS dp
                 JOIN ingreso AS i ON i.nhc = dp.nhc
                 JOIN centro_salud AS cs ON dp.id_centro_salud = cs.id_centro_salud
-                WHERE i.fecha_ingreso <= :fin
-                AND (i.fecha_alta IS NULL OR i.fecha_alta >= :inicio)
-                GROUP BY cs.descripcion;
+                WHERE i.fecha_ingreso <= '2025-12-31'
+                AND (i.fecha_alta IS NULL OR i.fecha_alta >= '2024-12-31')
+                GROUP BY cs.codigo_centro;
             ");
         }
         public function get_paciente_c() {
             $stmt = $this->pdo->prepare("
                 SELECT 
                     c.descripcion AS convivencia,
-                    COUNT(*) AS num_pacientes
+                    ROUND(COUNT(*) * 100.0 / (
+                        SELECT COUNT(*)
+                        FROM ingreso AS i2
+                        JOIN (
+                            SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                            FROM ingreso
+                            GROUP BY nhc
+                        ) AS ultimos2 
+                        ON i2.nhc = ultimos2.nhc AND i2.fecha_ingreso = ultimos2.fecha_max
+                        WHERE i2.fecha_ingreso <= :fin
+                        AND (i2.fecha_alta IS NULL OR i2.fecha_alta >= :inicio)
+                    ), 2) AS porcentaje
                 FROM datos_paciente AS dp
                 JOIN ingreso AS i ON i.nhc = dp.nhc
                 JOIN convivencia AS c ON dp.id_convivencia = c.id_convivencia
@@ -57,7 +81,18 @@
             $stmt = $this->pdo->prepare("
                 SELECT 
                     pc.descripcion AS ppal_cuidador,
-                    COUNT(*) AS num_pacientes
+                    ROUND(COUNT(*) * 100.0 / (
+                        SELECT COUNT(*)
+                        FROM ingreso AS i2
+                        JOIN (
+                            SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                            FROM ingreso
+                            GROUP BY nhc
+                        ) AS ultimos2 
+                        ON i2.nhc = ultimos2.nhc AND i2.fecha_ingreso = ultimos2.fecha_max
+                        WHERE i2.fecha_ingreso <= :fin
+                        AND (i2.fecha_alta IS NULL OR i2.fecha_alta >= :inicio)
+                    ), 2) AS porcentaje
                 FROM datos_paciente AS dp
                 JOIN ingreso AS i ON i.nhc = dp.nhc
                 JOIN ppal_cuidador AS pc ON dp.id_ppal_cuidador = pc.id_ppal_cuidador
@@ -70,7 +105,18 @@
             $stmt = $this->pdo->prepare("
                 SELECT 
                     as.descripcion AS ayuda_social,
-                    COUNT(*) AS num_pacientes
+                    ROUND(COUNT(*) * 100.0 / (
+                        SELECT COUNT(*)
+                        FROM ingreso AS i2
+                        JOIN (
+                            SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                            FROM ingreso
+                            GROUP BY nhc
+                        ) AS ultimos2 
+                        ON i2.nhc = ultimos2.nhc AND i2.fecha_ingreso = ultimos2.fecha_max
+                        WHERE i2.fecha_ingreso <= :fin
+                        AND (i2.fecha_alta IS NULL OR i2.fecha_alta >= :inicio)
+                    ), 2) AS porcentaje
                 FROM datos_paciente AS dp
                 JOIN ingreso AS i ON i.nhc = dp.nhc
                 JOIN ayuda_social AS as ON dp.id_centro_salud = as.id_ayuda_social
@@ -83,7 +129,18 @@
             $stmt = $this->pdo->prepare("
                 SELECT 
                     mi.descripcion AS motivo_inc,
-                    COUNT(*) AS num_pacientes
+                    ROUND(COUNT(*) * 100.0 / (
+                        SELECT COUNT(*)
+                        FROM ingreso AS i2
+                        JOIN (
+                            SELECT nhc, MAX(fecha_ingreso) AS fecha_max
+                            FROM ingreso
+                            GROUP BY nhc
+                        ) AS ultimos2 
+                        ON i2.nhc = ultimos2.nhc AND i2.fecha_ingreso = ultimos2.fecha_max
+                        WHERE i2.fecha_ingreso <= :fin
+                        AND (i2.fecha_alta IS NULL OR i2.fecha_alta >= :inicio)
+                    ), 2) AS porcentaje
                 FROM datos_paciente AS dp
                 JOIN ingreso AS i ON i.nhc = dp.nhc
                 JOIN motivo_inc AS mi ON dp.id_motivo_inc = mi.id_motivo_inc
